52c723bed53e55a7b2e5ae4650829322d7036bf6
Takashi Kakinuma <kakinuma.takashi@homes.ecweb.jp>
2019-07-18 09:58:06 +0900

    0718

--- SocketServerSample/Form1.cs
+++ SocketServerSample/Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using SocketServerSample.util;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +19,40 @@ namespace SocketServerSample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPEndPoint ipAdd = new IPEndPoint(IPAddress.Parse("192.168.11.3"), 8888);
+            TcpListener listener = new TcpListener(ipAdd);
+            listener.Start(0);
+            Console.WriteLine("Port:8888のListenを開始しました。");
+
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("クライアントが接続しました。");
+
+            if (client.Connected)
+            {
+                listener.Stop();
+                NetworkStream netStream = client.GetStream();
+                StreamReader sReader = new StreamReader(netStream, Encoding.UTF8);
+
+                string str = String.Empty;
+
+                do
+                {
+                    str = sReader.ReadLine();
+                    if (null == str)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(str);
+                } while (!str.Equals("quit"));
+                sReader.Close();
+                client.Close();
+            }
+            Console.WriteLine("終了するには、Enterキーを押してください");
+            Console.ReadLine();
         }
     }
 }
---
+++ SocketServerSample/util/Log.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SocketServerSample.util
+{
+    public static class Log
+    {
+        public static string i(string msg)
+        {
+            return write("I", msg);
+        }
+
+        public static string w(string msg)
+        {
+            return write("W", msg);
+        }
+
+        public static string e(string msg)
+        {
+            return write("E", msg);
+        }
+
+        public static string d(string msg)
+        {
+#if DEBUG
+            return write("D", msg);
+#else
+            return "";
+#endif
+        }
+
+        private static string write(string level, string msg)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff");
+            int processId = System.Diagnostics.Process.GetCurrentProcess().Id;
+            int threadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+
+            string log = String.Format("{0} [{1:D5}-{2:D5}]/{3}: {4}", timestamp, processId, threadId, level, msg);
+            Console.WriteLine(log);
+
+            return log;
+        }
+    }
+}
